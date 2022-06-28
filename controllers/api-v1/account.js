c382f1ed0d2e4413d84eafe5278d2dac3e6cf088
seiplet93 <seiplet93@gmail.com>
2022-06-28 18:31:43 -0400

    updated task route, added get route
    Co-authored-by: Eduarte783 <Eduarte783@users.noreply.github.com>
    Co-authored-by: Mitchell Marino <mitchdmarino@gmail.com>
    Co-authored-by: emilykiss <emilykiss@users.noreply.github.com>

--- controllers/api-v1/account.js
+++ controllers/api-v1/account.js
@@ -1,113 +1,116 @@
-const router = require('express').Router()
-const db = require('../../models')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const authLockedRoute = require('./authLockedRoute')
+const router = require("express").Router();
+const db = require("../../models");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const authLockedRoute = require("./authLockedRoute");
 
+// POST /accounts/register -- CREATE a new account
+router.post("/register", async (req, res) => {
+  try {
+    /// check if the account exists already
+    const findAccount = await db.Account.findOne({
+      email: req.body.email,
+    });
 
-// POST /accounts/register -- CREATE a new account 
-router.post('/register', async (req, res) => {
-    try {
-        /// check if the account exists already 
-        const findAccount = await db.Account.findOne({
-            email: req.body.email
-        })
-
-        // disallow accounts from registerring twice
-        if (findAccount) {
-            return res.status(400).json({ msg: 'email exists already' })
-        }
-        // hash the account's password
-        const password = req.body.password
-        const saltRounds = 12
-        const hashedPassword = await bcrypt.hash(password, saltRounds)
-
-        //create a new account with the hashed password 
-        const newAccount = new db.Account({
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        })
-        await newAccount.save()
-        // sign the account in 
-        // create the jwt payload
-        const payload = {
-            name: newAccount.name,
-            email: newAccount.email,
-            id: newAccount.id
-        }
-        // sign the token and send it back 
-        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1d' }) // expires in one day
-        res.json({ token })
-    } catch (err) {
-        console.warn(err)
-        // handle validation errors
-        if (err.name === 'ValidationError') {
-            res.status(400).json({ msg: err.message })
-        } else {
-            // handle all other errors
-            res.status(500).json({ msg: 'server error 500 😡' })
-        }
-        res.status(500).json(err)
-
+    // disallow accounts from registerring twice
+    if (findAccount) {
+      return res.status(400).json({ msg: "email exists already" });
     }
-})
+    // hash the account's password
+    const password = req.body.password;
+    const saltRounds = 12;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    //create a new account with the hashed password
+    const newAccount = new db.Account({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    await newAccount.save();
+    // sign the account in
+    // create the jwt payload
+    const payload = {
+      name: newAccount.name,
+      email: newAccount.email,
+      id: newAccount.id,
+    };
+    // sign the token and send it back
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    }); // expires in one day
+    res.json({ token });
+  } catch (err) {
+    console.warn(err);
+    // handle validation errors
+    if (err.name === "ValidationError") {
+      res.status(400).json({ msg: err.message });
+    } else {
+      // handle all other errors
+      res.status(500).json({ msg: "server error 500 😡" });
+    }
+    res.status(500).json(err);
+  }
+});
 
 // POST /accounts/login -- validate the login credentials
-router.post('/login', async (req, res) => {
-    try {
-        // all the data will come in on the req.body
-        console.log(req.body)
-        // try to find the account in the database 
-        const findAccount = await db.Account.findOne({
-            email: req.body.email
-        })
+router.post("/login", async (req, res) => {
+  try {
+    // all the data will come in on the req.body
+    console.log(req.body);
+    // try to find the account in the database
+    const findAccount = await db.Account.findOne({
+      email: req.body.email,
+    });
 
-        // if the account is not found, send a status of 400 and let the account know login failed 
-        if (!findAccount) {
-            return res.status(400).json({ msg: 'Invalid Login' })
-        }
-        // console.log(findaccount)
-        // Check if the supplied password matches the hash in the db 
-        const passwordCheck = await bcrypt.compare(req.body.password, findAccount.password )
-        // console.log(passwordCheck)
-        // if they do not match, return and let the account know that login has failed 
-        if (!passwordCheck) {
-            return res.status(400).json({msg: 'Invalid Login '})
-        }
-        // Create a jwt payload 
-        const payload = {
-            name: findAccount.name,
-            email: findAccount.email,
-            id: findAccount.id
-        } 
-        // sign the jwt and send it back 
-        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1d' }) // expires in one day
-        res.json({ token })
-       
-    } catch (err) {
-        console.log(err)
+    // if the account is not found, send a status of 400 and let the account know login failed
+    if (!findAccount) {
+      return res.status(400).json({ msg: "Invalid Login" });
     }
-})
+    // console.log(findaccount)
+    // Check if the supplied password matches the hash in the db
+    const passwordCheck = await bcrypt.compare(
+      req.body.password,
+      findAccount.password
+    );
+    // console.log(passwordCheck)
+    // if they do not match, return and let the account know that login has failed
+    if (!passwordCheck) {
+      return res.status(400).json({ msg: "Invalid Login " });
+    }
+    // Create a jwt payload
+    const payload = {
+      name: findAccount.name,
+      email: findAccount.email,
+      id: findAccount.id,
+    };
+    // sign the jwt and send it back
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    }); // expires in one day
+    res.json({ token });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 // GET /auth-locked -- checks accounts credentials and only sends back privilaged information if account is logged in properly
-router.get('/auth-locked', authLockedRoute, (req, res) => {
-    console.log('the current account is ', res.locals.account)
-    res.json({ msg: 'welcome to the secret auth-locked route 🕵🏻‍♂️' })
-    
-})
+router.get("/auth-locked", authLockedRoute, (req, res) => {
+  console.log("the current account is ", res.locals.account);
+  res.json({ msg: "welcome to the secret auth-locked route 🕵🏻‍♂️" });
+});
 
-router.get('/:id', authLockedRoute, async (req,res) => {
-    try {
-        console.log(req.params.id)
-        const account = await db.Account.findById(req.params.id).populate('tasks').populate('profiles')
-        res.json(account)
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({msg: 'server error'})
-    }
-})
+router.get("/:id", authLockedRoute, async (req, res) => {
+  try {
+    console.log(req.params.id);
+    const account = await db.Account.findById(req.params.id)
+      .populate("tasks")
+      .populate("profiles");
+    res.json(account);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "server error" });
+  }
+});
 
-
-
-module.exports = router 
+module.exports = router;
--- controllers/api-v1/tasks.js
+++ controllers/api-v1/tasks.js
@@ -1,62 +1,78 @@
-const router = require('express').Router()
-const db = require('../../models')
-const authLockedRoute = require('./authLockedRoute')
+const router = require("express").Router();
+const db = require("../../models");
+const authLockedRoute = require("./authLockedRoute");
 
-router.post('/', authLockedRoute, async (req,res) => {
+router.post("/", authLockedRoute, async (req, res) => {
+  try {
+    // find the account that is signed in
+    const account = res.locals.account;
+    // create a new task based on the req.body
+    const newTask = await db.Task.create(req.body);
+    // add the newtask to the account
+    account.tasks.push(newTask);
+    // save
+    await account.save();
+    await newTask.save();
+    const response = await account.populate("tasks");
+    res.json(response);
+  } catch (err) {
+    console.warn(err);
+    res.status(500).json({ msg: "server error" });
+  }
+});
+
+router.post(
+  "/:taskId/profile/:profileId",
+  authLockedRoute,
+  async (req, res) => {
     try {
-        // find the account that is signed in
-        const account = res.locals.account
-        // create a new task based on the req.body
-        const newTask = await db.Task.create(req.body)
-        // add the newtask to the account 
-        account.tasks.push(newTask)
-        // save 
-        await account.save()
-        await newTask.save()
-        const response = await account.populate('tasks')
-        res.json(response)
-    } catch (err) {
-        console.warn(err)
-        res.status(500).json({msg: 'server error'})
-    }
-})
+      const profile = await db.Profile.findById(req.params.profileId);
+      const task = await db.Task.findById(req.params.taskId);
 
-router.post('/:taskId/profile/:profileId', authLockedRoute, async (req,res) => {
-    try {
-        const profile = await db.Profile.findById(req.params.profileId)
-        const task = await db.Task.findById(req.params.taskId)
+      task.profile = profile;
+      await task.save();
+      console.log(task.profile, "here??");
+      res.json(task);
+    } catch (error) {}
+  }
+);
 
-        task.profile = profile
-        await task.save()
-    } catch (error) {
-        
-    }
-})
+router.get("/", authLockedRoute, async (req, res) => {
+  try {
+    const account = res.locals.account;
+    const tasks = await db.Task.find({
+      account: account._id,
+    }).populate("profile");
+    res.json(tasks);
+    console.log(tasks, "asdf");
+  } catch (err) {
+    console.warn(err);
+  }
+});
 
-router.put('/:id', async (req,res) => {
-    try {
-        const id = req.params.id
-        const options = {new: true}
-        // find the task 
-        const task = await db.Task.findByIdAndUpdate(id, req.body, options)
-        res.json(task)
-        
-    } catch (err) {
-        console.warn(err)
-        res.status(500).json({msg: 'server error'})
-    }
-})
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const options = { new: true };
+    // find the task
+    const task = await db.Task.findByIdAndUpdate(id, req.body, options);
+    res.json(task);
+  } catch (err) {
+    console.warn(err);
+    res.status(500).json({ msg: "server error" });
+  }
+});
 
-router.delete('/:id', async (req,res) => {
-    try {
-        const id = req.params.id
-        await db.Task.findByIdAndDelete(id)
-        // no content status
-        res.sendStatus(204)
-    } catch (err) {
-        console.warn(err)
-        res.status(500).json({msg: 'server error'})
-    }
-})
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    await db.Task.findByIdAndDelete(id);
+    // no content status
+    res.sendStatus(204);
+  } catch (err) {
+    console.warn(err);
+    res.status(500).json({ msg: "server error" });
+  }
+});
 
-module.exports = router
+module.exports = router;
